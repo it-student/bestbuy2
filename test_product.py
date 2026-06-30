b9d6847b1140290bdcb7c1bc9bd07e516ecc6b02
Yasin Akkus <akkusoft@gmail.com>
2026-06-30 13:35:11 +0200

    Changed the test_product.py to include tests of the derived Promotion-Subclasses. All passing successfully.

--- test_product.py
+++ test_product.py
@@ -2,7 +2,10 @@
 This module is a pytest file for testing the products.py files code functionalities.
 """
 import pytest
+
+from main import second_half_price
 from products import Product
+from promotions import SecondHalfPrice, ThirdOneFree, PercentDiscount
 
 def test_creating_products():
     # Empty name
@@ -33,3 +36,21 @@ def test_buy_too_much():
     test_prod = Product("MacBook Air M2", price=1450, quantity=1)
     with pytest.raises(ValueError, match="Error while making order! Quantity larger than what exists"):
         test_prod.buy(3)
+
+def test_second_half_price_promoted_buy():
+    test_prod = Product("MacBook Air M2", price=1450, quantity=10)
+    test_second_half_price = SecondHalfPrice("Second Half Price")
+    test_prod.set_promotion(test_second_half_price)
+    assert test_prod.buy(2) == 2175
+
+def test_third_one_free_promotion():
+    test_prod = Product("MacBook Air M2", price=1450, quantity=10)
+    test_third_one_free = ThirdOneFree("Third One Free Price")
+    test_prod.set_promotion(test_third_one_free)
+    assert test_prod.buy(3) == 2900
+
+def test_percent_discount():
+    test_prod = Product("MacBook Air M2", price=1450, quantity=10)
+    test_thirty_three_percent_discount = PercentDiscount("Percent Discount", 33)
+    test_prod.set_promotion(test_thirty_three_percent_discount)
+    assert test_prod.buy(3) == 2914.5
